8dffe61feb8c79c398ecbd3debf7fefb1e68c404
Saheb Giri <iamsahebgiri>
2021-09-10 08:49:22 +0530

    👌 IMPROVE: navbar with toggle

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("hello")
+const menu = document.getElementById('menu');
+const menuIcon = document.getElementById('menu-icon');
+const mobileNavbar = document.getElementById('mobile-navbar-wrapper');
+
+let isOpen = false;
+
+menu.addEventListener('click', (e) => {
+  if (isOpen) {
+    menuIcon.src = 'img/icons/menu-icon.svg';
+    mobileNavbar.style.display = 'none';
+  } else {
+    menuIcon.src = 'img/icons/x-icon.svg';
+    mobileNavbar.style.display = 'block';
+  }
+  isOpen = !isOpen;
+});
