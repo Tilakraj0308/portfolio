227d13d5de873a788e2bf86ee1605487f555a1ef
Saheb Giri <iamsahebgiri>
2021-09-19 12:35:00 +0530

    📦 NEW: add basic service worker for pwa

--- js/app.js
+++ js/app.js
@@ -1,20 +1,38 @@
-const menu = document.getElementById('menu');
-const menuIcon = document.getElementById('menu-icon');
-const mobileNavbar = document.getElementById('mobile-navbar-wrapper');
+const menu = document.getElementById('menu')
+const menuIcon = document.getElementById('menu-icon')
+const mobileNavbar = document.getElementById('mobile-navbar-wrapper')
 
-let isOpen = false;
+let isOpen = false
 
 menu.addEventListener('click', function () {
   if (isOpen) {
-    menuIcon.src = 'img/icons/menu-icon.svg';
-    mobileNavbar.style.display = 'none';
+    menuIcon.src = 'img/icons/menu-icon.svg'
+    mobileNavbar.style.display = 'none'
   } else {
-    menuIcon.src = 'img/icons/x-icon.svg';
-    mobileNavbar.style.display = 'block';
+    menuIcon.src = 'img/icons/x-icon.svg'
+    mobileNavbar.style.display = 'block'
   }
-  isOpen = !isOpen;
-});
+  isOpen = !isOpen
+})
 
 // https://assets2.lottiefiles.com/packages/lf20_138qcknv.json Manger yellow
 // https://assets7.lottiefiles.com/packages/lf20_ocrcnofw.json Loki
 // https://assets8.lottiefiles.com/packages/lf20_1KjmRZ.json Developer
+
+// registering service worker
+
+window.addEventListener('load', () => {
+  registerSW()
+})
+
+// Register the Service Worker
+async function registerSW() {
+  if ('serviceWorker' in navigator) {
+    try {
+      await navigator.serviceWorker.register('js/serviceworker.js')
+    } catch (e) {
+      console.log(e)
+      console.log('SW registration failed')
+    }
+  }
+}
